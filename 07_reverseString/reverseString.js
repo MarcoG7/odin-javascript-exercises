5f26f193dd2d4b08d4e9a38e4903b5d54af16e2a
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-14 17:15:32 +0300

    Do ex7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const str_len = string.length;
+    let res_str = "";
+    for (let i = str_len-1; i >= 0; i--) {
+        res_str += string[i];
+    }
+    return res_str;
 };
 
 // Do not edit below this line
