76c315ec8a8320613c9c130bb2861248367f2d5f
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-14 17:13:16 +0300

    Do ex6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) { return "ERROR"; }
+    let res_string = "";
+    for (let i = 0; i < num; i++) {
+        res_string += string;
+    }
+    return res_string;
 };
 
 // Do not edit below this line
