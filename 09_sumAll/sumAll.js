8f1673267ca9180a865126be3b4d98441970173b
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-14 17:49:55 +0300

    Do ex9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (a < 0 || b < 0) { return "ERROR"; }
+    if (typeof a !== "number" || typeof b !== "number") { return "ERROR"; }
+    if (Math.floor(a) != a || Math.floor(b) != b) { return "ERROR"; }
+
+    const nb_termes = Math.abs(b - a) + 1;
+    return (nb_termes * (b + a)) / 2;
 };
 
 // Do not edit below this line
