d277fe246c7c0a9dcc0a80c901187b4372857fca
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-19 13:12:28 +0300

    Do ex16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldObj, obj) => {
+        let objAge;
+        let prevAge;
+        if (!obj.yearOfDeath) {
+            objAge = new Date().getFullYear() - obj.yearOfBirth;
+        } else {
+            objAge = obj.yearOfDeath - obj.yearOfBirth;
+        }
+        if (!oldObj.yearOfDeath) {
+            prevAge = new Date().getFullYear() - oldObj.yearOfBirth;
+        } else {
+            prevAge = oldObj.yearOfDeath - oldObj.yearOfBirth;
+        }
 
+        if (objAge > prevAge) {
+            return obj;
+        } else {
+            return oldObj;
+        }
+    })
 };
 
 // Do not edit below this line
