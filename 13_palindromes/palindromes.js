3d5b0ab799e2d43c31fa39c0720d4b7f85fea119
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-19 12:19:22 +0300

    Do ex13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const newStr = str
+        .toLowerCase()
+        .split("")
+        .filter((character) => alphanumerical.includes(character))
+        .join("");
+    
+    const reversedStr = newStr.split("").reverse().join("");
+
+    return newStr === reversedStr;
 };
 
 // Do not edit below this line
