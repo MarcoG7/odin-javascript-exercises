812bff2286152ef62cee8a2869876e6f0826a1fe
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-19 12:40:23 +0300

    Do ex14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let num2 = +num;
 
+    if (num2 < 0) {
+        return "OOPS";
+    }
+
+    if (num2 === 0) {
+        return 0;
+    }
+
+    if (num2 === 1) {
+        return 1;
+    }
+
+    let fib =  [1, 1]
+    while (fib.length != num2) {
+        fib.push(fib[fib.length-1] + fib[fib.length-2]);
+    }
+
+    return fib[fib.length-1];
 };
 
 // Do not edit below this line
