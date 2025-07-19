bb282ae8535d4133f102d0db8f35e6a6d6893f1e
Marc Gerges <63096791+MarcoG7@users.noreply.github.com>
2025-07-19 12:59:08 +0300

    Do ex15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.map((book) => book.title);
+    
+    // let titles = [];
+    // for (book of books) {
+    //     titles.push(book.title);
+    // }
+    // return titles;
 };
 
 // Do not edit below this line
